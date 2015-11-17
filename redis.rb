523ccea7d352088a7c3e038d49745ca4fc614cc2
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-11-17 17:49:08 -0600

    Demo

--- redis.rb
+++ redis.rb
@@ -3,20 +3,20 @@ require 'open-uri'
 require 'redis'
 
 data = JSON.parse(open("https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD").read)
+puts data
+#connection = Redis.new(:host => "ec2-54-83-9-36.compute-1.amazonaws.com", :port => 7369, :url => "redis://h:pcucqtm555pilc58fo5s91vojp1@ec2-54-83-9-36.compute-1.amazonaws.com:7369", :password => "pcucqtm555pilc58fo5s91vojp1")
 
-connection = Redis.new(:host => "ec2-54-83-9-36.compute-1.amazonaws.com", :port => 7369, :url => "redis://h:pcucqtm555pilc58fo5s91vojp1@ec2-54-83-9-36.compute-1.amazonaws.com:7369", :password => "pcucqtm555pilc58fo5s91vojp1")
+#i = 1
 
-i = 1
+#while i < 101
 
-while i < 101
-
-	redis.hset "row#{i}","id",i
-	redis.hset "row#{i}","Ethnicity", data[i][8]
-	redis.hset "row#{i}","sex", data[i][9]
-	redis.hset "row#{i}","CauseOfDeath", data[i][10]
-	redis.hset "row#{i}","year", data[i][7]
-	puts redis.hgetall "row#{i}"
-	i += 1
-end
+#	redis.hset "row#{i}","id",i
+#	redis.hset "row#{i}","Ethnicity", data[i][8]
+#	redis.hset "row#{i}","sex", data[i][9]
+#	redis.hset "row#{i}","CauseOfDeath", data[i][10]
+#	redis.hset "row#{i}","year", data[i][7]
+#	puts redis.hgetall "row#{i}"
+#	i += 1
+#end
 
 puts "Data Stored"
