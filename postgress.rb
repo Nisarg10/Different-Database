0b02809a84de19dcf225f1c0962c5bc93ce82734
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-11-17 03:41:57 -0600

    Demo

--- postgress.rb
+++ postgress.rb
@@ -15,10 +15,10 @@ connection.exec("CREATE TABLE DeathCause(id INTEGER PRIMARY KEY, Ethnicity TEXT,
 i = 1
 
 while i < 101 
-	Ethnicity = data[i][8]
-	sex = data[i][9]
-	CauseOfDeath = data[i][10]
-	Year = data[i][7]	 	
+	Ethnicity = data[#{i}][8]
+	sex = data[#{i}][9]
+	CauseOfDeath = data[#{i}][10]
+	Year = data[#{i}][7]	 	
 	connection.exec("insert into DeathCause VALUES('#{i})','#{Ethnicity}','#{sex}', '#{CauseOfDeath}', #{Year}")
 	i += 1
 end
