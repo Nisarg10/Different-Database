c826bc7ec0a4964334539fdfba42eff3d3698ce2
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-11-17 03:37:17 -0600

    Demo

--- postgress.rb
+++ postgress.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 
 data = JSON.parse(open("https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD").read)
 
-connection = PG.connect  :dbname => "dnq31r5ahvg4b",
+connection = PG.connect  :dbname => "dfm1q8av5dgnd5",
 			  :user => "dckxytovmhkaqf",
                          :password => "TCiCqyCh-WqUb1FDNEmFACeM7h",
                          :host => "ec2-54-204-25-54.compute-1.amazonaws.com"
