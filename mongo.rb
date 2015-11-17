3249b698156af53fc2bea9007ea1dbb679afd9f6
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-11-17 04:25:55 -0600

    Demo

--- mongo.rb
+++ mongo.rb
@@ -5,12 +5,12 @@ include Mongo
 
 data = JSON.parse(open("https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD").read)
 
-connection = Mongo::Client.new(['ds049854.mongolab.com:49854'], :database => 'heroku_fxxhzprk', :user => 'heroku_fxxhzprk')
+connection = Mongo::Client.new(['ds047712.mongolab.com:47712/heroku_hfts48pr'], :database => 'heroku_hfts48pr', :user => ‘nsd6275’, :password => ‘nsd6275’)
 
 i = 1
 
 while i < 101 
-	n = connection[:heroku_fxxhzprk].insert_one({
+	n = connection[: heroku_hfts48pr].insert_one({
 		id: "#{i}",
 		ethnicity: data[i][8],
 		sex: data[i][9],
