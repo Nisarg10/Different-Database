4a1505c2186649749d3ec9574e27803106f84649
Kishan Bhavesh Vekaria <Kishan@Kishans-MacBook-Pro.local>
2015-11-17 18:02:11 -0600

    Demo

--- postgress.rb
+++ postgress.rb
@@ -10,7 +10,7 @@ connection = PG.connect  :dbname => "dfm1q8av5dgnd5",
                          :password => "TCiCqyCh-WqUb1FDNEmFACeM7h",
                          :host => "ec2-54-204-25-54.compute-1.amazonaws.com"
 
-
+connection.exec "DROP TABLE IF EXISTS DeathCause"
 
 connection.exec "CREATE TABLE DeathCause(id INTEGER PRIMARY KEY, Ethnicity TEXT, sex Text, CauseOfDeath TEXT, Year INTEGER)"
 
